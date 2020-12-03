162bba7dc92d2bcaf23fc3d3708de7ee45ce2998
MichaelTrzaskoma <74200729+MichaelTrzaskoma@users.noreply.github.com>
2020-12-03 18:22:55 -0500

    GenProfileUpload/Update
    This file contains the general methods used to upload a record to the firestore and updating a profile utilizes the same method! Contains pyzipcode module to convert zip code to state for proper terms. Terms field is to be pulled and used for querying.

---
+++ backend/FireStorm & Binary Conversion Test/ProfileUploadGeneral.py
@@ -0,0 +1,223 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from datetime import datetime
+from datetime import date
+from pyzipcode import ZipCodeDatabase
+zcdb = ZipCodeDatabase()
+
+cred = credentials.Certificate("testing-845eb-firebase-adminsdk-cckoe-bf65029977.json")
+firebase_admin.initialize_app(cred)
+
+
+db = firestore.client()
+
+user_Ref =db.collection("User")
+table_Ref = db.collection("Index Table").document("Terms")
+refList = table_Ref.get().to_dict().get('Terms')
+
+#Used to initiation user or update their information, specify when using optional attributes
+def updtUser(userEmail, gender, dob, zipC, gpa, major = ' ', race = ' ', religion = ' ', dissabilities = ' ', sat = ' ', act = ' ', address1 = ' ', address2 = ' ' ):
+    
+    list1 = [gender]
+    list1.append(catAge(dob))
+    list1.append(catState(zipC))
+    list1.append(catGPA(gpa))
+
+    list2 = [race, religion, dissabilities]
+    for i in range(len(list2)):
+        if list2[i] != ' ':
+            list1.append(list2[i])
+
+    if (sat != ' '):
+        list1.append(catSat(sat))
+    if (act != ' '):
+        list1.append(catAct(act))
+
+    binary = setBin(list1)
+
+    user_Ref.document(userEmail).set({
+        u'Email' : userEmail,
+        u'Gender' : gender,
+        u'Date of Birth' : dob,
+        u'Zip' : zipC,
+        u'GPA' : gpa,
+        u'Major': major,
+        u'Relgion' : religion,
+        u'Dissabilities' : dissabilities,
+        u'SAT Score': sat,
+        u'ACT Score' : act,
+        u'Address 1' : address1,
+        u'Address 2' : address2,
+        u'Binary' : binary, 
+        u'Terms' : list1
+})
+   
+
+#def binaryConvert():
+    #refers to a list
+
+
+
+#def updateAccount():
+    #update all fields? not too sure    
+
+def catState(zipC):
+    zipcode = zcdb[zipC]
+    state = abrevConv[zipcode.state]
+    return state
+
+abrevConv = {    
+    'AL': 'Alabama', 
+    'AK': 'Alaska', 
+    'AS': 'American Samoa', 
+    'AZ': 'Arizona', 
+    'AR': 'Arkansas', 
+    'CA': 'California', 
+    'CO': 'Colorado', 
+    'CT': 'Connecticut', 
+    'DE': 'Delaware', 
+    'DC': 'District of Columbia', 
+    'FL': 'Florida', 'GA': 'Georgia', 
+    'GU': 'Guam', 
+    'HI': 'Hawaii', 
+    'ID': 'Idaho', 
+    'IL': 'Illinois', 
+    'IN': 'Indiana', 
+    'IA': 'Iowa', 
+    'KS': 'Kansas', 
+    'KY': 'Kentucky', 
+    'LA': 'Louisiana',
+    'ME': 'Maine', 
+    'MD': 'Maryland', 
+    'MA': 'Massachusetts', 
+    'MI': 'Michigan', 
+    'MN': 'Minnesota',
+    'MS': 'Mississippi', 
+    'MO': 'Missouri', 
+    'MT': 'Montana', 
+    'NE': 'Nebraska', 
+    'NV': 'Nevada', 
+    'NH': 'New Hampshire', 
+    'NJ': 'New Jersey', 
+    'NM': 'New Mexico', 
+    'NY': 'New York', 
+    'NC': 'North Carolina', 
+    'ND': 'North Dakota', 
+    'MP': 'Northern Mariana Islands', 
+    'OH': 'Ohio', 
+    'OK': 'Oklahoma', 
+    'OR': 'Oregon', 
+    'PA': 'Pennsylvania', 
+    'PR': 'Puerto Rico', 
+    'RI': 'Rhode Island', 
+    'SC': 'South Carolina', 
+    'SD': 'South Dakota', 
+    'TN': 'Tennessee', 
+    'TX': 'Texas', 
+    'UT': 'Utah', 
+    'VT': 'Vermont', 
+    'VI': 'Virgin Islands', 
+    'VA': 'Virginia', 
+    'WA': 'Washington', 
+    'WV': 'West Virginia', 
+    'WI': 'Wisconsin', 
+    'WY': 'Wyoming'
+}
+
+
+
+def catGPA(gpa):
+    str = ' '
+    gpa = float(gpa)
+    if (gpa >= 1.0) & (gpa <= 2.0):
+        str = 'Minimum Grade Point Average From 1.0 to 2.0'
+    elif (gpa >= 2.1) & (gpa <= 2.5):
+        str = 'Minimum Grade Point Average From 2.1 to 2.5' 
+    elif (gpa >= 2.6) & (gpa <= 3.0):
+        str = 'Minimum Grade Point Average From 2.6 to 3.0' 
+    elif (gpa >= 3.1) & (gpa <= 3.5):
+        str = 'Minimum Grade Point Average From 3.1 to 3.5' 
+    elif (gpa >= 3.6) & (gpa <= 4.0):
+        str = 'Minimum Grade Point Average From 3.6 to 4.0' 
+    return str
+
+def catAge(dob):
+    birth = datetime.strptime(dob, "%m/%d/%Y")
+    today = date.today()
+    age = today.year - birth.year - ((today.month, today.day) < (birth.month, birth.day))
+    string = ''
+    if age < 13:
+        string = ("Age 13")
+    elif age > 30:
+        string = ("Age Greater Than 30")
+    else:
+        string = ("Age "+ str(age))
+    return string
+
+
+def catSat(sat):
+    str = ''
+    if (float(sat) >= 400) & (float(sat) <= 1000):
+        str = 'SAT Scores From 400 To 1,000'
+    elif (float(sat) >= 1001) & (float(sat) <= 1200):
+        str = 'SAT Scores From 1,001 To 1,200'
+    elif (float(sat) >= 1201) & (float(sat) <= 1400):
+        str = 'SAT Scores From 1,201 To 1,400'
+    elif (float(sat) >= 1401) & (float(sat) <= 1600):
+        str = 'SAT Scores From 1,401 To 1,600'
+    return str
+
+def catAct(act):
+    str = ' '
+    if  (float(act) >= 10) & (float(act) <=15):
+        str = 'ACT Scores From 10 To 15'
+    elif  (float(act) >= 16) & (float(act) <=20):
+        str = 'ACT Scores From 16 To 20'
+    elif  (float(act) >= 21) & (float(act) <=25):
+        str = 'ACT Scores From 21 To 25'
+    elif  (float(act) >= 26) & (float(act) <=30):
+        str = 'ACT Scores From 26 To 30'
+    elif  (float(act) >= 31):
+        str = 'ACT Scores Greater Than 31'
+    return str
+
+#splits the string to array    
+#input -> string
+#output -> char array
+def split(word):
+	return [char for char in word]
+
+
+#converts list to string
+#Input -> list
+#output -> array
+def toString(list):
+    str = ""
+    return (str.join(list))
+
+
+#Finds the index of the word based on a table generated from the driver
+#Input -> string, the word
+#output -> int, the index
+def catIndex(word):
+    ind = refList.index(word)
+    return ind
+
+def setBin(list):
+    binaryInitial = '0'* 807
+    usrList = split(binaryInitial)
+    for i in range(len(list)):
+        ind = catIndex(list[i])
+        usrList[ind] = "1" 
+    binaryStr = toString(usrList)
+    return binaryStr
+
+#Updates the binary based on the word in firestore
+#Input -> int, string, the id of the scholarship and the term
+#Output -> none
+
+
+listA = table_Ref.get().to_dict().get('Terms')
+#print(setBin(['Accounting']))
+updtUser('some email', 'Male', '5/11/1999', '10308', '3.6', sat = '1400')
