2dc3cad34cf8f350ac0f6ea2aeee024f5b9c448e
MichaelTrzaskoma <74200729+MichaelTrzaskoma@users.noreply.github.com>
2020-12-03 20:54:33 -0500

    Finalized Filter File
    This file should be the final iteration of the filtering, for now, might have better ideas

--- backend/FireStorm & Binary Conversion Test/Filtering_BinCosSim.py
+++ backend/FireStorm & Binary Conversion Test/Filtering_BinCosSim.py
@@ -1,6 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+
 #CosSim Calc
 from numpy import dot
 from numpy.linalg import norm
@@ -10,33 +11,20 @@ firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
-scholar_ref=db.collection(u"Scholarships2")
+scholar_ref=db.collection(u"ScholarshipHub")
 user_ref = db.collection(u'Users')
-
-#Update the document reference with the current user id
-user = user_ref.document(u'VS02Ok5RyWD63BrbnzFY')
-
-#Playing with getting the term list 
-#doc_info = user.get(field_paths={'Name'}).to_dict().get('Name')
-#term_list = user.get().to_dict().get('Terms')
-#print(term_list)
-
-
-
-#if doc_info.exists:
- #   print(f'Document data: {doc_info')
-#else:
- #   print(u'No such document!')
 
 ##########################################     METHODS    #######################
 # Methods for binary filtering
 
+#Split method
 #Input -> String, ideally the binary string
 #Output -> integer list to be used for CosSim
 def split(word):
     stringlist = [char for char in word]
     intlist = [int(i) for i in stringlist]
     return intlist
+
 
 #For the CosSim comparison
 #Input -> two strings, user binary and scholarship binary
@@ -47,19 +35,38 @@ def comparison(user_bin, input_bin):
     cos_sim = dot(queryList, inputList)/(norm(queryList)*norm(inputList))
     return cos_sim
 
+
 #For filtereing after a query is done, returns a list of id's that we can loop through to pull info of those scholarships
-#Input -> Query generator object, filtering number
+#Input -> Query generator object, string user id, filtering float number
 #Output -> List of strings, these are id's that can be used to pull information  
-def filter_results(query, filterVal):
+def filter_results(userId):
+    filterVal = 0.4
+    user = user_ref.document(userId).get().to_dict()
+    userTerms = user.get('Terms')
+    query  = scholar_ref.where(u'Terms', u'array_contains_any', userTerms).stream()
+    filteredScholar = []
+    userBin = user.get('Binary') 
     for i in query:
-        #print(' {} => {}'.format(i.id, i.get('GPA')))
-        scholBin = i.get('Binary')
-        value = comparison(scholBin, userBin)
-        if(value > filterVal):
-            scholid = i.id
-            filteredSchol.append(scholid)
-    return filteredSchol
-#
+        scholarBin = i.get('Binary')
+        if binCompare(userBin, scholarBin) == True:
+            value = comparison(scholarBin, userBin)
+            if(value > filterVal):
+                scholarid = i.id
+                filteredScholar.append(scholarid)
+    return filteredScholar
+
+#Method to compare and see hard conditions
+#Input - > 2 strings, user binary, scholarship binary
+#Output - > boolean, true if good, false if bad
+def binCompare(user_bin, scholar_bin):
+    userList = split(user_bin)
+    scholarList = split(scholar_bin)
+    for y in range(len(userList)):
+        if userList[y] < scholarList[y]:
+            return False
+    return True
+
+#Method to return content of a specific scholarship, id will be supplied from a list of id's that is from filtering in filter_reults method
 #Input -> String, Scholarship id
 #Output -> List, filled of the scholarship's contents 
 def getInfo(scholarId):
@@ -74,13 +81,8 @@ def getInfo(scholarId):
     scholarInfo.append(scholarDir.get('Contact Info'))
     return scholarInfo
 
-print(getInfo('JPa6dkCtt8doqLDkMp2m'))
-##################################################################################
-# update this properly with example binary comparison 
-userBin = user.get().to_dict().get('Binary')
+############################### Testing Stuff #########################
 
-#This is the query, modify it a bit more based on user fields
-query = scholar_ref.where(u'Terms', u'array_contains_any', [u'Something']).stream()
 
-filterVal = 0.5
-filteredSchol = []
+#Example for comparisons 
+#print(filter_results('some email'))
